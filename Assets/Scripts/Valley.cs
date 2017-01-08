fd684124480ab69e265ac36d1323a4af1a3a7fa1
Tian Xia <cruisky@gmail.com>
2017-01-08 09:05:10 -0500

    valley animations

---
+++ Assets/Scripts/Valley.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TX;
+using TX.Game;
+
+public class Valley : BaseBehaviour {
+
+	public Sprite BuffedSprite;
+
+	public Transform BuffAnimation;
+
+	[InspectorButton("Buff")]
+	void StartBuff(){
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		renderer.sprite = BuffedSprite;
+
+		BuffAnimation.gameObject.SetActive(true);
+	}
+}
